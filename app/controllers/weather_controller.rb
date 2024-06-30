a4275d40df55723390168f18f7c5091608aadf6f
Xuebing Li <xuebinglee@gmail.com>
2024-07-01 00:04:40 +0900

    Add span events for OpenWeather API calls

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,10 +1,16 @@
 class WeatherController < ApplicationController
   def current
     city = params[:city]
+    span = OpenTelemetry::Trace.current_span
+    span.set_attribute('city', city)
     open_weather = OpenWeather.new
+    span.add_event('Fetching geo coordinate')
     coordinate = open_weather.geo_coordinate(city)
+    span.add_event('Fetched geo coordinate')
     if coordinate
+      span.add_event('Fetching current weather')
       current_weather = open_weather.current_weather(coordinate)
+      span.add_event('Fetched current weather')
       render json: current_weather
     else
       render json: { error: "City #{city} not found" }, status: :not_found
